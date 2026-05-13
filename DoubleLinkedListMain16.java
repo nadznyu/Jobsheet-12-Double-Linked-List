b822e2a2581aef619186e532cec0733d73867626
nadznyu <nadiaalya0107@gmail.com>
2026-05-13 18:27:16 +0700

    class Main

---
+++ DoubleLinkedListMain16.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class DoubleLinkedListMain16 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DoubleLinkedList16 list = new DoubleLinkedList16();
+        int pilihan;
+
+        do {
+            System.out.println("\n===== MENU DOUBLE LINKED LIST =====");
+            System.out.println("1. Tambah data di awal");
+            System.out.println("2. Tambah data di akhir");
+            System.out.println("3. Sisipkan data di tengah (setelah NIM)");
+            System.out.println("4. Hapus data di awal");
+            System.out.println("5. Hapus data di akhir");
+            System.out.println("6. Tampilkan data");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu : ");
+            pilihan = scan.nextInt();
+            scan.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    Mahasiswa16 mhsAwal = DoubleLinkedList16.inputMahasiswa(scan);
+                    list.addFirst(mhsAwal);
+                    break;
+                case 2:
+                    Mahasiswa16 mhsAkhir = DoubleLinkedList16.inputMahasiswa(scan);
+                    list.addLast(mhsAkhir);
+                    break;
+                case 3:
+                    System.out.print("Masukkan NIM yang dicari : ");
+                    String keyNim = scan.nextLine();
+                    System.out.print("\nMasukkan data baru");
+                    System.out.println();
+                    Mahasiswa16 dataBaru = DoubleLinkedList16.inputMahasiswa(scan);
+                    list.insertAfter(keyNim, dataBaru);
+                    break;
+                case 4:
+                    list.removeFirst();
+                    break;
+                case 5:
+                    list.removeLast();
+                    break;
+                case 6:
+                    list.print();
+                    break;
+                case 0:
+                    System.out.println("Program selesai.");
+                    break;
+                default:
+                    System.out.println("Menu tidak valid.");
+            }
+        } while (pilihan != 0);
+        scan.close();
+    }
+}
